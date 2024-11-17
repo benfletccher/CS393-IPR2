425f523c97a8f3ce8a87d67ac76a9c6309abb5ae
Matei Golesteanu <matei.golesteanu@westpoint.edu>
2024-11-17 17:41:39 -0500

    added path

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cadet_list', views.cadet_list, name='cadets'),
+    path('product_list', views.product_list, name='products')
 ]
