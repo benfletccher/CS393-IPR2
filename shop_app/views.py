1ccc24fa146263fceddf25ace0a7273b69ed3668
Benjamin Fletcher <benjamin.fletcher@westpoint.edu>
2024-11-17 16:33:43 -0500

    initial wedserver running

--- shop_app/views.py
+++ shop_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+#from .forms import Hours
+#^^ update for this repos folder
+#from .models import HoursLogged
+##^^ update for this repos folder
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Welcome to the Cadet ONline Trading Post')
